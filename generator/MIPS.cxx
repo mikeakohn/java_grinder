a537d22c657ee8e20a7d8acb4ed00f70f72f29dc
Michael Kohn <mike@mikekohn.net>
2014-01-12 17:33:33 -0600

    Add the MIPS files for real this time.

---
+++ generator/MIPS.cxx
@@ -0,0 +1,239 @@
+/**
+ *  Java Grinder
+ *  Author: Michael Kohn
+ *   Email: mike@mikekohn.net
+ *     Web: http://www.mikekohn.net/
+ * License: GPL
+ *
+ * Copyright 2014 by Michael Kohn
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "MIPS.h"
+
+// ABI is:
+// r4
+// r5
+// r6
+// r7
+// r8
+// r9
+// r10
+// r11
+
+MIPS::MIPS()
+{
+
+}
+
+MIPS::~MIPS()
+{
+
+}
+
+int MIPS::open(char *filename)
+{
+  return 0;
+}
+
+void MIPS::serial_init()
+{
+}
+
+void MIPS::method_start(int local_count, const char *name)
+{
+}
+
+void MIPS::method_end(int local_count)
+{
+}
+
+int MIPS::push_integer(int32_t n)
+{
+  return -1;
+}
+
+int MIPS::push_integer_local(int index)
+{
+  return -1;
+}
+
+int MIPS::push_long(int64_t n)
+{
+  return -1;
+}
+
+int MIPS::push_float(float f)
+{
+  return -1;
+}
+
+int MIPS::push_double(double f)
+{
+  return -1;
+}
+
+int MIPS::push_byte(int8_t b)
+{
+  return -1;
+}
+
+int MIPS::push_short(int16_t s)
+{
+  return -1;
+}
+
+int MIPS::pop_integer_local(int index)
+{
+  return -1;
+}
+
+int MIPS::pop()
+{
+  return -1;
+}
+
+int MIPS::dup()
+{
+  return -1;
+}
+
+int MIPS::dup2()
+{
+  return -1;
+}
+
+int MIPS::swap()
+{
+  return -1;
+}
+
+int MIPS::add_integers()
+{
+  return -1;
+}
+
+int MIPS::sub_integers()
+{
+  return -1;
+}
+
+int MIPS::mul_integers()
+{
+  return -1;
+}
+
+int MIPS::div_integers()
+{
+  return -1;
+}
+
+int MIPS::neg_integer()
+{
+  return -1;
+}
+
+int MIPS::shift_left_integer()
+{
+  return -1;
+}
+
+int MIPS::shift_right_integer()
+{
+  return -1;
+}
+
+int MIPS::shift_right_uinteger()
+{
+  return -1;
+}
+
+int MIPS::and_integer()
+{
+  return -1;
+}
+
+int MIPS::or_integer()
+{
+  return -1;
+}
+
+int MIPS::xor_integer()
+{
+  return -1;
+}
+
+int MIPS::inc_integer(int index, int num)
+{
+  return -1;
+}
+
+int MIPS::jump_cond(const char *label, int cond)
+{
+  return -1;
+}
+
+int MIPS::jump_cond_integer(const char *label, int cond)
+{
+  return -1;
+}
+
+int MIPS::return_local(int index, int local_count)
+{
+  return -1;
+}
+
+int MIPS::return_integer(int local_count)
+{
+  return -1;
+}
+
+int MIPS::return_void(int local_count)
+{
+  return -1;
+}
+
+int MIPS::jump(const char *name)
+{
+  return -1;
+}
+
+int MIPS::call(const char *name)
+{
+  return -1;
+}
+
+int MIPS::invoke_static_method(const char *name, int params, int is_void)
+{
+  return -1;
+}
+
+int MIPS::brk()
+{
+  return -1;
+}
+
+#if 0
+void MIPS::close()
+{
+}
+#endif
+
+// GPIO functions
+int MIPS::ioport_setPinsAsInput(int port) { return -1; }
+int MIPS::ioport_setPinsAsOutput(int port) { return -1; }
+int MIPS::ioport_setPinsValue(int port) { return -1; }
+int MIPS::ioport_setPinsHigh(int port) { return -1; }
+int MIPS::ioport_setPinsLow(int port) { return -1; }
+int MIPS::ioport_setPinAsOutput(int port) { return -1; }
+int MIPS::ioport_setPinAsInput(int port) { return -1; }
+int MIPS::ioport_setPinHigh(int port) { return -1; }
+int MIPS::ioport_setPinLow(int port) { return -1; }
+int MIPS::ioport_isPinInputHigh(int port) { return -1; }
+int MIPS::ioport_getPortInputValue(int port) { return -1; }
+int MIPS::ioport_setPortOutputValue(int port) { return -1; }
+
---
+++ generator/MIPS.h
@@ -0,0 +1,79 @@
+/**
+ *  Java Grinder
+ *  Author: Michael Kohn
+ *   Email: mike@mikekohn.net
+ *     Web: http://www.mikekohn.net/
+ * License: GPL
+ *
+ * Copyright 2014 by Michael Kohn
+ *
+ */
+
+#ifndef _MIPS_H
+#define _MIPS_H
+
+#include "Generator.h"
+
+class MIPS : public Generator
+{
+public:
+  MIPS();
+  virtual ~MIPS();
+
+  virtual int open(char *filename);
+
+  virtual void serial_init();
+  virtual void method_start(int local_count, const char *name);
+  virtual void method_end(int local_count);
+  virtual int push_integer(int32_t n);
+  virtual int push_integer_local(int index);
+  virtual int push_long(int64_t n);
+  virtual int push_float(float f);
+  virtual int push_double(double f);
+  virtual int push_byte(int8_t b);
+  virtual int push_short(int16_t s);
+  virtual int pop_integer_local(int index);
+  virtual int pop();
+  virtual int dup();
+  virtual int dup2();
+  virtual int swap();
+  virtual int add_integers();
+  virtual int sub_integers();
+  virtual int mul_integers();
+  virtual int div_integers();
+  virtual int neg_integer();
+  virtual int shift_left_integer();
+  virtual int shift_right_integer();
+  virtual int shift_right_uinteger();
+  virtual int and_integer();
+  virtual int or_integer();
+  virtual int xor_integer();
+  virtual int inc_integer(int index, int num);
+  virtual int jump_cond(const char *label, int cond);
+  virtual int jump_cond_integer(const char *label, int cond);
+  virtual int return_local(int index, int local_count);
+  virtual int return_integer(int local_count);
+  virtual int return_void(int local_count);
+  virtual int jump(const char *name);
+  virtual int call(const char *name);
+  virtual int invoke_static_method(const char *name, int params, int is_void);
+  virtual int brk();
+  //virtual void close();
+
+  // GPIO functions
+  virtual int ioport_setPinsAsInput(int port);
+  virtual int ioport_setPinsAsOutput(int port);
+  virtual int ioport_setPinsValue(int port);
+  virtual int ioport_setPinsHigh(int port);
+  virtual int ioport_setPinsLow(int port);
+  virtual int ioport_setPinAsOutput(int port);
+  virtual int ioport_setPinAsInput(int port);
+  virtual int ioport_setPinHigh(int port);
+  virtual int ioport_setPinLow(int port);
+  virtual int ioport_isPinInputHigh(int port);
+  virtual int ioport_getPortInputValue(int port);
+  virtual int ioport_setPortOutputValue(int port);
+};
+
+#endif
+
