56ef99cf8de0b7e34365be96843d9c9f897673a0
Mortis69 <joe_7@sbcglobal.net>
2014-03-02 08:36:27 -0600

    fixed baload bug

--- common/compile.cxx
+++ common/compile.cxx
@@ -635,7 +635,7 @@ printf("code_len=%d\n", code_len);
 
       case 51: // baload (0x33)
         if (operand_stack_ptr == 0)
-        { ret = -1; }
+        { ret = generator->array_read_byte(); }
           else
         { ret = array_load(java_class, generator, operand_stack[--operand_stack_ptr], ARRAY_TYPE_BYTE); }
 
