5e786a30f5af8ea6f8704808b9d1ba08d3d1ece3
Michael Kohn <mike@mikekohn.net>
2014-03-15 20:44:15 -0500

    Tests: Adding static int array test.

---
+++ tests/StaticIntArrays.java
@@ -0,0 +1,25 @@
+
+public class StaticIntArrays
+{
+  static int[] array = { 1, 2, 3, 4 };
+
+  static public int add_nums()
+  {
+    int total = 0;
+    int n;
+
+    for (n = 0; n < array.length; n++)
+    {
+      total += array[n];
+    }
+
+    return total;
+  }
+
+  static public void main(String args[])
+  {
+    add_nums();
+  }
+}
+
+
