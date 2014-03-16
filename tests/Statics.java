15dd4016d51d3fa3c40a7bcf6c8bfe577630c668
Michael Kohn <mike@mikekohn.net>
2014-03-16 18:03:08 -0500

    Adding Statics test.

---
+++ tests/Statics.java
@@ -0,0 +1,20 @@
+
+public class Statics
+{
+  static int integer = 50;
+  static short short_integer = 25;
+  static byte byte_integer = 1;
+
+  static public int get_number()
+  {
+    return integer + short_integer + byte_integer;
+  }
+
+  static public void main(String args[])
+  {
+    short_integer = 20;
+    get_number();
+  }
+}
+
+
