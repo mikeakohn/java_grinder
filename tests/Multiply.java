4bb4c434204b02660661522a61253545a62928c7
Michael Kohn <mike@mikekohn.net>
2014-03-19 17:21:35 -0500

    Adding a multiply test.  Going to add a bigger one later.

---
+++ tests/Multiply.java
@@ -0,0 +1,15 @@
+
+public class Multiply
+{
+  static public int mul_nums(int a, int b)
+  {
+    return a * b;
+  }
+
+  static public void main(String args[])
+  {
+    mul_nums(5, 6);
+  }
+}
+
+
