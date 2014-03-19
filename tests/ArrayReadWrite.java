942c6add44e0baa8380d42b377dba6021b4130c4
Michael Kohn <mike@mikekohn.net>
2014-03-18 21:16:52 -0500

    Adding a array malloc / read / write test.

---
+++ tests/ArrayReadWrite.java
@@ -0,0 +1,31 @@
+
+public class ArrayReadWrite
+{
+  static public int add_nums()
+  {
+    int[] array = new int[6];
+    int total = 0;
+    int n;
+
+    array[0] = 1;
+    array[1] = -2;
+    array[2] = 3;
+    array[3] = 10;
+    array[4] = 11;
+    array[5] = 12;
+
+    for (n = 0; n < array.length; n++)
+    {
+      total += array[n];
+    }
+
+    return total;
+  }
+
+  static public void main(String args[])
+  {
+    add_nums();
+  }
+}
+
+
