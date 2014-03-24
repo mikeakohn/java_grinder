1f30569e741cbba63884af6fe2a9c8b72e6c1ec7
Michael Kohn <mike@mikekohn.net>
2014-03-23 19:09:08 -0500

    Adding a divide test.

---
+++ tests/Divide.java
@@ -0,0 +1,15 @@
+
+public class Divide
+{
+  static public int div_nums(int a, int b)
+  {
+    return a / b;
+  }
+
+  static public void main(String args[])
+  {
+    div_nums(27000, 400);
+  }
+}
+
+
