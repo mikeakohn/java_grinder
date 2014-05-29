d20ce381cb8bd91aecdcbafa296f295cde2a3810
Michael Kohn <mike@mikekohn.net>
2014-05-28 20:12:20 -0500

    Tests: Adding loop test.

---
+++ tests/LoopTest.java
@@ -0,0 +1,37 @@
+
+public class LoopTest
+{
+  static public int loop_up()
+  {
+     int total = 0;
+     int n;
+
+     for (n = 0; n < 640; n++)
+     {
+       total += 1;
+     }
+
+     return total;
+  }
+
+  static public int loop_down()
+  {
+     int total = 0;
+     int n;
+
+     for (n = 640; n > 0; n--)
+     {
+       total += 1;
+     }
+
+     return total;
+  }
+
+  static public void main(String args[])
+  {
+     //loop_up();
+     loop_down();
+  }
+}
+
+
