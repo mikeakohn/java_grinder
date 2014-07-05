109e658c64838ce7d53d7858b2acda38d13a294b
Michael Kohn <mike@mikekohn.net>
2014-07-05 17:50:26 -0500

    Automated tests for having a local variable pointing to a global array.

---
+++ tests/ArrayPointers.java
@@ -0,0 +1,32 @@
+
+public class ArrayPointers
+{
+  static byte[] array1 = { 1, 2, 3, 4 };
+  static byte[] array2 = { 5, 6, 7, 8, 9 };
+
+  static public int reflection(int i) { return i; }
+
+  static public void main(String args[])
+  {
+    byte[] array;
+    int total = 0;
+    int i;
+
+    array = array1;
+    for (i = 0; i < array.length; i++)
+    {
+      total += array[i];
+    }
+
+    array = array2;
+    for (i = 0; i < array.length; i++)
+    {
+      total += array[i];
+    }
+
+    // force r15 to hold the value of total
+    total = reflection(total);
+  }
+}
+
+
