38eb6b1cb88674aedda8fec4d338dfd195cb569c
Michael Kohn <mike@mikekohn.net>
2014-12-12 19:45:59 -0600

    Adding UART sample program.

---
+++ samples/misc/UARTTest.java
@@ -0,0 +1,24 @@
+
+import net.mikekohn.java_grinder.UART0;
+
+public class UARTTest
+{
+  static void main(String args[])
+  {
+    int c;
+
+    UART0.init(UART0.BAUD_9600);
+
+    while(true)
+    {
+      while(!UART0.isDataAvailable());
+
+      c = UART0.read();
+
+      while(!UART0.isSendReady());
+
+      UART0.send(c);
+    }
+  }
+}
+
