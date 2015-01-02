d96f6ce3edad003e5abe1fcee6be3cdbe67a6ea0
Michael Kohn <mike@mikekohn.net>
2015-01-02 05:48:51 -0600

    x86: Add sample programs.

---
+++ samples/x86/Test.java
@@ -0,0 +1,79 @@
+
+public class Test
+{
+  static int glo = 10;
+
+  public static int add_nums(int a, int b)
+  {
+    int c;
+    c = a + b;
+
+    return c;
+  }
+
+  public static int sub_nums(int a, int b)
+  {
+    int c;
+    c = a - b;
+
+    return c;
+  }
+
+  public static int mul_nums(int a, int b)
+  {
+    int c;
+    c = a * b;
+
+    return c;
+  }
+
+  public static int div_nums(int a, int b)
+  {
+    int c;
+    c = a / b;
+
+    return c;
+  }
+
+  public static int mod_nums(int a, int b)
+  {
+    int c;
+    c = a % b;
+
+    return c;
+  }
+
+  public static int shift_nums(int a, int b)
+  {
+    int c;
+
+    c = a << b;
+
+    return c;
+  }
+
+  public static int loop_test(int a, int b)
+  {
+    int c,d;
+
+    d = 0;
+
+    for (c = 0; c < b; c++)
+    {
+      d = d + a;
+    }
+
+    return d;
+  }
+
+  public static void put_static(int a)
+  {
+    glo = a;
+  }
+
+  public static int get_static()
+  {
+    return glo;
+  }
+}
+
---
+++ samples/x86/test_java.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init();
+int add_nums_II(int a, int b);
+int sub_nums_II(int a, int b);
+int mul_nums_II(int a, int b);
+int div_nums_II(int a, int b);
+int mod_nums_II(int a, int b);
+int shift_nums_II(int a, int b);
+int loop_test_II(int a, int b);
+void put_static_I(int a);
+int get_static();
+
+int main(int argc, char *argv[])
+{
+  int a,b;
+
+  if (argc != 3)
+  {
+    printf("Usage: %s <num> <num>\n", argv[0]);
+    exit(0);
+  }
+
+  a = atoi(argv[1]);
+  b = atoi(argv[2]);
+
+  init();
+
+  printf("%d + %d = %d\n", a, b, add_nums_II(a,b));
+  printf("%d - %d = %d\n", a, b, sub_nums_II(a,b));
+  printf("%d * %d = %d\n", a, b, mul_nums_II(a,b));
+  printf("%d / %d = %d\n", a, b, div_nums_II(a,b));
+  printf("%d %% %d = %d\n", a, b, mod_nums_II(a,b));
+  printf("%d << %d = %d\n", a, b, shift_nums_II(a,b));
+  printf("loop test (%d,%d)  %d\n", a, b, loop_test_II(a,b));
+  printf("get_static()=%d\n", get_static());
+  printf("put_static(%d)\n", a);
+  put_static_I(a);
+  printf("get_static()=%d\n", get_static());
+  return 0;
+}
+
