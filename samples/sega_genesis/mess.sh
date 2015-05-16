7da4628589e861fff93d0e969794f5aa2cfc41ed
Michael Kohn <mike@mikekohn.net>
2015-05-16 11:40:49 -0500

    Sega Genesis: Adding file for starting mess.

---
+++ samples/sega_genesis/mess.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+extra=""
+
+if [ "$1" = "debug" ]
+then
+  extra="-debug"
+fi
+
+if [ "$1" = "avi" ]
+then
+  extra="-aviwrite out.avi"
+fi
+
+mess genesis -window \
+  -cart1 sega_genesis_java_demo.bin \
+  ${extra}
+
+if [ "$1" = "avi" ]
+then
+  mencoder -oac mp3lame -ovc x264 -o video.avi -vf scale=640:480 ~/.mess/snap/out.avi
+fi
+
