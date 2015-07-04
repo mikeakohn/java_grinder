a13d87c3242a820f8728e5446fa33562bf86276d
Michael Kohn <mike@mikekohn.net>
2015-07-04 12:34:35 -0500

    Sega Genesis: Adding Google Go program to convert a Z80 binary file into Java code that can be loaded with the Sega Genesis API.

---
+++ samples/sega_genesis/bin2java.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+import "os"
+import "strconv"
+
+func main() {
+  fmt.Println("bin2java - Copyright 2015 by Michael Kohn")
+
+  if len(os.Args) != 2 {
+    fmt.Println("Usage: " + os.Args[0] + " <binfile>")
+    os.Exit(0)
+  }
+
+  // Open wav file for reading
+  fmt.Println("Reading: " + os.Args[1])
+
+  file_in, err := os.Open(os.Args[1])
+
+  if err != nil {
+    panic(err)
+  }
+
+  // Close the file if the program exits
+  defer file_in.Close()
+
+  stat, err := file_in.Stat()
+  if err != nil {
+    panic(err)
+  }
+
+  file_size := int(stat.Size())
+
+  fmt.Println(strconv.Itoa(file_size))
+
+  data := make([]byte, file_size)
+  file_in.Read(data)
+
+  fmt.Println("public class BinFile\n{")
+  fmt.Println("  public static byte[] code =\n  {")
+
+  for i := 0; i < len(data); i++ {
+    if i % 8 == 0 {
+      fmt.Print("\n   ")
+    }
+
+    d := fmt.Sprintf("%4d", int8(data[i]))
+
+    fmt.Print(" " + d + ",")
+  }
+
+  fmt.Println("\n  };")
+  fmt.Println("}\n")
+}
+
+
