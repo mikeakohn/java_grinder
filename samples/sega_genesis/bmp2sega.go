1f4ea94f21984c80943061a7827dfa6ca9159e5f
Michael Kohn <mike@mikekohn.net>
2015-07-09 18:07:25 -0500

    Sega Genesis: Conversion from BMP to Sega hardware appears to be working.

--- samples/sega_genesis/bmp2sega.go
+++ samples/sega_genesis/bmp2sega.go
@@ -82,15 +82,23 @@ func readBmpInfo(file_in *os.File) uint32 {
   return width * uint32(height) * 3
 }
 
-func writeByteArray(data []int) {
+func writeByteArray(data []uint8) {
   for i := 0; i < len(data); i++ {
     if i % 8 == 0 {
       fmt.Print("\n   ")
     }
 
-    d := fmt.Sprintf("0x%03x", int16(data[i]))
+    fmt.Printf(" 0x%02x,", data[i])
+  }
+}
 
-    fmt.Print(" " + d + ",")
+func writeInt16Array(data []int) {
+  for i := 0; i < len(data); i++ {
+    if i % 8 == 0 {
+      fmt.Print("\n   ")
+    }
+
+    fmt.Printf(" 0x%03x,", data[i])
   }
 }
 
@@ -104,49 +112,115 @@ func getPixelLocation(x int, y int) int {
   return (x * 3) + (y * 320 *3)
 }
 
+func doesPatternMatch(a []uint8, b []uint8) bool {
+  if len(a) != len(b) { fmt.Println("Problem in match") }
+
+  for i := 0; i < len(a); i++ {
+    if a[i] != b[i] { return false; }
+  }
+
+  return true
+}
+
 func readData(file_in *os.File, image_size uint32) uint32 {
   var loc int
   var color int
   var palette_map map[int]int
-  //dict Dict or map?
+  var pattern_nybble int
+  var patterns [][]uint8
+  var image []uint8
+  //var pattern_byte
 
   fmt.Println("readData: " + strconv.Itoa(int(image_size)))
 
   data := make([]uint8, image_size)
   palette := make([]int, 16)
+  //patterns := make([][]uint8, 0)
+  //image := make([]uint8, 40 * 28)
+  //image := make([]uint8, 0)
   palette_map = make(map[int]int)
   color_count := 0
 
   file_in.Read(data)
 
   fmt.Println("public class ClassName\n{")
-  fmt.Println("  public static byte[] pattern =\n  {")
 
-  for y := 0; y < 224; y = y + 8 {
-    for x := 0; x < 320; x = x + 8 {
-      loc = getPixelLocation(x, y)
-      color = getPixelColor(data[loc:loc+3])
-      _, ok := palette_map[color]
+  // Ouch.  There's faster ways to do this, but this should be pretty
+  // quick anyway, so who cares.  For each 8x8 block of the image, set up
+  // an 8x8 pixel x 4 bit color, 32 byte pattern.  To save ROM space, do
+  // not duplicate patterns.
+  for y0 := 0; y0 < 224; y0 = y0 + 8 {
+    for x0 := 0; x0 < 320; x0 = x0 + 8 {
 
-      if !ok {
-        if color_count == 16 {
-          fmt.Println("Too many colors in image")
-          return 0
+      pattern := make([]uint8, 32)
+      pattern_nybble = 0
+
+      // Get 8x8 pattern out of image
+      for y := 0; y < 8; y++ {
+        for x := 0; x < 8; x++ {
+          loc = getPixelLocation(x0 + x, y0 + y)
+          color = getPixelColor(data[loc:loc+3])
+          _, ok := palette_map[color]
+
+          if !ok {
+            if color_count == 16 {
+              fmt.Println("Too many colors in image")
+              return 0
+            }
+            palette_map[color] = color_count
+            palette[color_count] = color
+            color_count += 1
+          }
+
+          color = palette_map[color]
+
+          if (pattern_nybble & 1) == 0 {
+            pattern[pattern_nybble >> 1] = uint8(color) << 4
+          } else {
+            pattern[pattern_nybble >> 1] |= uint8(color)
+          }
+
+          pattern_nybble++
         }
-        palette_map[color] = color_count
-        palette[color_count] = color
-        color_count += 1
+      }
+
+      match := false
+
+      // Check if this pattern already exists
+      for index, element := range patterns {
+        //fmt.Printf("%q\n", element)
+        if doesPatternMatch(element, pattern) {
+          image = append(image, uint8(index))
+          match = true
+          break
+        }
+      }
+
+      if match == false {
+        image = append(image, uint8(len(patterns)))
+        patterns = append(patterns, pattern)
       }
     }
   }
 
-  fmt.Println("\n  };")
+  if len(image) != 40 * 28 {
+    fmt.Printf("Image isn't 40x28? %d\n", len(image))
+    return 0
+  }
 
-  fmt.Println("  public static byte[] image =\n  {")
-  fmt.Println("\n  };")
+  fmt.Println("  public static byte[] pattern =\n  {")
+  for index, pattern := range patterns {
+    fmt.Print("    // Pattern %d\n", index)
+    writeByteArray(pattern)
+  }
+  fmt.Println("\n  };\n")
+
+  fmt.Print("  public static byte[] image =\n  {")
+  writeByteArray(image)
+  fmt.Println("\n  };\n")
 
   fmt.Print("  public static byte[] palette =\n  {")
-  writeByteArray(palette)
+  writeInt16Array(palette)
   fmt.Println("\n  };")
 
   fmt.Println("}\n")
