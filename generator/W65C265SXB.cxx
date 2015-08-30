378bcee04cb1bbb0e0ffda9b1d89573ee104bf34
Mortis69 <joe_7@sbcglobal.net>
2015-08-30 15:02:36 -0500

    added target

--- generator/W65C265SXB.cxx
+++ generator/W65C265SXB.cxx
@@ -27,9 +27,53 @@ W65C265SXB::~W65C265SXB()
 
 int W65C265SXB::open(const char *filename)
 {
-  if (W65816::open(filename) != 0) { return -1; }
+  if(Generator::open(filename) != 0) { return -1; }
+
+  fprintf(out, ".65816\n");
+
+  // java stack
+  fprintf(out, "stack equ 0x0200\n");
+
+  // ram start
+  fprintf(out, "ram_start equ 0x7000\n");
+  fprintf(out, "heap_ptr equ ram_start\n");
+
+  // points to locals
+  fprintf(out, "locals equ 0x00\n");
+
+  // temp variables
+  fprintf(out, "result equ 0x02\n");
+  fprintf(out, "remainder equ 0x04\n");
+  fprintf(out, "length equ 0x06\n");
+  fprintf(out, "value1 equ 0x08\n");
+  fprintf(out, "value2 equ 0x10\n");
+  fprintf(out, "value3 equ 0x12\n");
+  fprintf(out, "address equ 0x14\n");
+
+  // start
+  fprintf(out, ".org 0x0300\n");
+  fprintf(out, "; change to 16-bit mode\n");
+  fprintf(out, "  clc\n");
+  fprintf(out, "  xce\n");
+  fprintf(out, "; all 16-bit registers\n");
+  fprintf(out, "  rep #0x30\n");
+  fprintf(out, "; set up processor stack\n");
+  fprintf(out, "  lda #0x1FF\n");
+  fprintf(out, "  tcs\n");
+  fprintf(out, "; set up direct-page\n");
+  fprintf(out, "  pea 0x0000\n");
+  fprintf(out, "; clear java stack\n");
+  fprintf(out, "  lda #0\n");
+  fprintf(out, "  ldx #0\n");
+  fprintf(out, "clear_java_stack:\n");
+  fprintf(out, "  sta stack,x\n");
+  fprintf(out, "  inx\n");
+  fprintf(out, "  inx\n");
+  fprintf(out, "  cpx #0x100\n");
+  fprintf(out, "  bne clear_java_stack\n");
+  fprintf(out, "; set up java stack pointer\n");
+  fprintf(out, "  ldx #0xFE\n");
 
   return 0;
 }
-
 
