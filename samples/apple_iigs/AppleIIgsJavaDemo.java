b89a6703d275de2ef04ac324c4e43a375e54184b
Mortis69 <joe_7@sbcglobal.net>
2015-09-05 12:24:04 -0500

    updated demo

--- samples/apple_iigs/AppleIIgsJavaDemo.java
+++ samples/apple_iigs/AppleIIgsJavaDemo.java
@@ -1,14 +1,31 @@
-
+import net.mikekohn.java_grinder.CPU;
 import net.mikekohn.java_grinder.AppleIIgs;
 
 public class AppleIIgsJavaDemo
 {
   static public void main(String args[])
   {
-    AppleIIgs.printChar('M');
-    AppleIIgs.printChar('I');
-    AppleIIgs.printChar('K');
-    AppleIIgs.printChar('E');
+    int i, j;
+
+    for(j = (int)'A'; j < (int)'Z'; j++) 
+    {
+      for(i = (int)'A'; i < (int)'Z'; i++) 
+      {
+        AppleIIgs.printChar((char)i);
+        AppleIIgs.printChar((char)j);
+        AppleIIgs.printChar((char)i);
+        AppleIIgs.printChar((char)j);
+        AppleIIgs.printChar((char)i);
+        AppleIIgs.printChar((char)j);
+      }
+    }
+
+//    AppleIIgs.printChar('M');
+//    AppleIIgs.printChar('I');
+//    AppleIIgs.printChar('K');
+//    AppleIIgs.printChar('E');
+
+//    CPU.asm("brk\n");
 
     while(true) { }
   }
