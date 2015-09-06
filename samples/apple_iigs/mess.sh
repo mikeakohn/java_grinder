e2964ed5dced73d1b0f2d672ce1ca422f2d566ac
Michael Kohn <mike@mikekohn.net>
2015-09-06 15:59:40 -0500

    Apple IIgs: Making it easier to compile and run the demo with some scripts.

---
+++ samples/apple_iigs/mess.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+extra=""
+
+if [ -e /storage/roms/apple2gs ]
+then
+  ROMPATH=/storage/roms
+else
+  ROMPATH=/usbdisk/roms
+fi
+
+
+if [ "$1" = "debug" ]
+then
+  extra="-debug"
+fi
+
+if [ "$1" = "avi" ]
+then
+  extra="-aviwrite out.avi"
+fi
+
+mess apple2gs -window -rompath ${ROMPATH} \
+  -flop1 apple_iigs_java_demo.dsk \
+  ${extra}
+
+if [ "$1" = "avi" ]
+then
+  mencoder -oac mp3lame -ovc x264 -o video.avi -vf scale=640:480 ~/.mess/snap/out.avi
+fi
+
