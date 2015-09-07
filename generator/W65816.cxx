6f136e50f6712d19012c50d7f01d7e3cab66ce40
Mortis69 <joe_7@sbcglobal.net>
2015-09-07 15:58:15 -0500

    insert array was using the wrong data type

--- generator/W65816.cxx
+++ generator/W65816.cxx
@@ -1076,14 +1076,22 @@ int W65816::new_array(uint8_t type)
 
 int W65816::insert_array(const char *name, int32_t *data, int len, uint8_t type)
 {
+  fprintf(out, ".align 16\n");
+
   if (type == TYPE_BYTE)
-  { return insert_db(name, data, len, TYPE_INT); }
+  {
+    return insert_db(name, data, len, TYPE_SHORT);
+  }
     else
   if (type == TYPE_SHORT)
-  { return insert_dw(name, data, len, TYPE_INT); }
+  {
+    return insert_dw(name, data, len, TYPE_SHORT);
+  }
     else
   if (type == TYPE_INT)
-  { return insert_dc32(name, data, len, TYPE_INT); }
+  {
+    return insert_dw(name, data, len, TYPE_SHORT);
+  }
 
   return -1;
 }
