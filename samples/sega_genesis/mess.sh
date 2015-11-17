68706ade50baf29319e52a99e3f7124780b9961e
Michael Kohn <mike@mikekohn.net>
2015-11-16 18:01:20 -0600

    Use ffmpeg instead of aviconv.

--- samples/sega_genesis/mess.sh
+++ samples/sega_genesis/mess.sh
@@ -19,7 +19,7 @@ mess genesis -window \
 if [ "$1" = "avi" ]
 then
   #mencoder -oac mp3lame -ovc x264 -o video.avi -vf scale=640:480 ~/.mess/snap/out.avi
-  avconv -i snap/out.avi -vf hflip -s 640x480 out.mp4
-  avconv -i out.mp4 -vf hflip out2.mp4
+  ffmpeg -i snap/out.avi -vf hflip -s 640x480 out.mp4
+  ffmpeg -i out.mp4 -vf hflip out2.mp4
 fi
 
