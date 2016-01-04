e29df24496126f009a94b41dd2654de50938365f
Mortis69 <joe_7@sbcglobal.net>
2016-01-03 20:15:06 -0600

    added musak and colors

--- samples/atari_2600/SpaceRevengeTitle.java
+++ samples/atari_2600/SpaceRevengeTitle.java
@@ -39,14 +39,54 @@ public class SpaceRevengeTitle
     0, 126, 126, 126, 6, 126, 6, 126, 
   };
 
+  // 11, 14, 19, 23. 29
+  public static byte bass_line[] = 
+  {
+    29, 23, 19, 14, 11, 14, 19, 23, 29, 23, 19, 14, 11, 11, 11, 11,
+    29, 23, 19, 14, 11, 14, 19, 23, 29, 23, 19, 14, 11, 11, 11, 11,
+    29, 23, 19, 14, 11, 14, 19, 23, 29, 23, 19, 14, 11, 11, 11, 11,
+    11, 11, 14, 0, 19, 19, 23, 0, 11, 11, 14, 0, 19, 19, 23, 0,
+    29, 23, 19, 14, 11, 14, 19, 23, 29, 23, 19, 14, 11, 11, 11, 11,
+    29, 23, 19, 14, 11, 14, 19, 23, 29, 23, 19, 14, 11, 11, 11, 11,
+    11, 11, 14, 0, 19, 19, 23, 0, 11, 11, 14, 0, 19, 19, 23, 0,
+    11, 11, 14, 0, 19, 19, 23, 0, 11, 11, 14, 0, 19, 19, 23, 0,
+  };
+
+  // 22, 25, 28
+  public static byte lead_line[] = 
+  {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    22, 22, 25, 0, 22, 22, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    22, 22, 25, 0, 22, 22, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    28, 0, 28, 0, 22, 0, 22, 0, 25, 0, 28, 0, 25, 0, 22, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    28, 22, 25, 0, 28, 22, 25, 0, 28, 22, 25, 0, 28, 28, 28, 0,
+    28, 22, 25, 0, 28, 22, 25, 0, 28, 22, 25, 0, 25, 25, 25, 0,
+    28, 22, 25, 0, 28, 22, 25, 0, 28, 22, 25, 0, 28, 25, 22, 0,
+  };
+
   public static void main()
   {
-    Atari2600.setTitlePos(0);
-    Atari2600.setTitleColor(0x2e);
+    int timer = 0;
+    int note = 0;
+    int temp0, temp1;
+    int color = 48;
+
+    Atari2600.setTitlePos(96);
   
+    Atari2600.setAudioControl0((byte)6);
+    Atari2600.setAudioControl1((byte)4);
+    
+    Atari2600.setAudioVolume0((byte)0);
+    Atari2600.setAudioVolume1((byte)0);
+
+    Atari2600.setAudioFrequency0((byte)0);
+    Atari2600.setAudioFrequency1((byte)0);
+
     while(true)
     {
       Atari2600.startVblank();
+      Atari2600.setTitleColor(color);
 
       if (Atari2600.isSwitchResetDown())
       {
@@ -56,6 +96,38 @@ public class SpaceRevengeTitle
         }
 
         Atari2600.setBank((byte)1);
+      }
+
+      timer++;
+      if(timer > 8)
+      {
+        timer = 0;
+        temp0 = bass_line[note];
+
+	if(temp0 == 0)
+          Atari2600.setAudioVolume0((byte)0);
+        else
+          Atari2600.setAudioVolume0((byte)15);
+
+        temp1 = lead_line[note];
+
+        if(temp1 == 0)
+          Atari2600.setAudioVolume1((byte)0);
+        else
+          Atari2600.setAudioVolume1((byte)15);
+
+        if(temp1 != 0)
+        {
+          color += 16;
+          if(color > 127)
+            color = 48;
+        }
+
+        Atari2600.setAudioFrequency0((byte)temp0);
+        Atari2600.setAudioFrequency1((byte)temp1);
+
+        note++;
+        note &= 127;
       }
 
       // set horizontal positions
