e764854ea22d5d21c7ecd73ac2278151704f3330
Mortis69 <joe_7@sbcglobal.net>
2016-01-24 13:39:43 -0600

    updated demo

--- samples/w65c265sxb/BlinkLED.java
+++ samples/w65c265sxb/BlinkLED.java
@@ -1,3 +1,5 @@
+// blinks LED on P40 if button on P41 is pressed
+
 import net.mikekohn.java_grinder.IOPort4;
 import net.mikekohn.java_grinder.W65C265SXB;
 
@@ -6,34 +8,36 @@ public class BlinkLED
   public static void main()
   {
     int i;
+    int temp = 5;
 
-    W65C265SXB.putChar('\n');
-    W65C265SXB.putInt(-32767);
-    W65C265SXB.putChar('\n');
-    W65C265SXB.putInt(367);
-    W65C265SXB.putChar('\n');
-    W65C265SXB.putInt(12345);
-    W65C265SXB.putChar('\n');
+    IOPort4.setPinsAsOutput(0b11111111);
+    IOPort4.setPinsValue(0);
 
-    W65C265SXB.putChar('\n');
-    IOPort4.setPinsAsOutput(255);
+    IOPort4.setPinsAsInput(0b00000010);
+
+    W65C265SXB.putString("\nWelcome to the LED Blinker!\n");
+    W65C265SXB.putString("(press Q to quit.)\n");
 
     while(true)
     {
-      W65C265SXB.putString("on\n");
-      IOPort4.setPinHigh(0);
-      
-      for(i = 0; i < 30000; i++)
-      {
-        // delay
-      }
+      if(W65C265SXB.getChar() == 'Q')
+        break;
 
-      W65C265SXB.putString("off\n");
-      IOPort4.setPinLow(0);
-      
-      for(i = 0; i < 30000; i++)
+      if(IOPort4.getPortInputValue() == 0)
       {
-        // delay
+        IOPort4.setPinHigh(0);
+      
+        for(i = 0; i < 3000; i++)
+        {
+          // delay
+        }
+
+        IOPort4.setPinLow(0);
+      
+        for(i = 0; i < 3000; i++)
+        {
+          // delay
+        }
       }
     }
   }
