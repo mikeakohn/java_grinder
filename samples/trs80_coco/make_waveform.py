c14cd005dfb22deb09ed790c14f2e20dee50df59
Michael Kohn <mike@mikekohn.net>
2016-01-31 11:34:23 -0600

    Added utility for creating sound waveforms on a TRS-80.

---
+++ samples/trs80_coco/make_waveform.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+
+rate = 8000
+samples = []
+
+if len(sys.argv) != 3:
+  print "Usage " + sys.argv[0] + " <freq> <sin/square/sawtooth>"
+  sys.exit(0)
+
+print "Rate is " + str(rate) + " samples a second"
+
+size = rate / 440
+
+if sys.argv[2] == "square":
+  for i in range(0, size):
+    if i < size / 2: samples.append(63)
+    else: samples.append(0)
+elif sys.argv[2] == "sawtooth":
+  dx = float(63) / float(size - 1)
+  sample = float(63)
+
+  for i in range(0, size):
+    samples.append(int(sample))
+    sample -= dx
+
+print "  public static byte[] waveform = {",
+for i in range(0, len(samples)):
+  if (i % 8) == 0: print "\n    ",
+  print " 0x%02x," % samples[i],
+print "  };"
+
