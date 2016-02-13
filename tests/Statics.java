5a447e3ee1b2c9f1a5ef061abdb45511789fcf2b
Michael Kohn <mike@mikekohn.net>
2016-02-13 17:32:30 -0600

    Adding a negative byte number into the statics.

--- tests/Statics.java
+++ tests/Statics.java
@@ -4,10 +4,11 @@ public class Statics
   static int integer = 50;
   static short short_integer = 25;
   static byte byte_integer = 1;
+  static byte byte_integer_neg = -50;
 
   static public int get_number()
   {
-    return integer + short_integer + byte_integer;
+    return integer + short_integer + byte_integer + byte_integer_neg;
   }
 
   static public void main(String args[])
