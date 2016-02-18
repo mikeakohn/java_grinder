8afbc921d3e2208975fd6cfb0ae757b868cc7dbb
Emiliano Vaz Fraga <efraga@gmail.com>
2016-02-18 02:33:41 -0200

    MSX Java main class and MSXHello sample.

---
+++ samples/msx/MSXHello.java
@@ -0,0 +1,35 @@
+/**
+ *  Java Grinder
+ *  Author: Michael Kohn
+ *   Email: mike@mikekohn.net
+ *     Web: http://www.naken.cc/
+ * License: GPL
+ *
+ * Copyright 2014-2016 by Michael Kohn
+ *
+ */
+
+/*
+ *   MSX support by Giovanni Nunes - https://github.com/plainspooky
+ *                  Emiliano Fraga - https://github.com/efraga-msx
+ */
+
+import net.mikekohn.java_grinder.MSX;
+
+public class MSXHello
+{
+  public static String strMsg = "Hello World";
+
+  public static void main(String args[])
+  {
+	MSX.cls();
+	
+	for (int i = 0; i < strMsg.length(); i++) {
+		MSX.putChar(strMsg.charAt(i));
+	}
+	
+	while(true);
+  }
+}
+
+
