4b6cb18ccd81cee8e893607464f082ffb1a2d234
Emiliano Vaz Fraga <efraga@gmail.com>
2016-02-18 02:33:12 -0200

    Adjustments to method CHECK defines and calls. Traces were included.

--- api/msx_.cxx
+++ api/msx_.cxx
@@ -11,6 +11,7 @@
 
 /*
  *   MSX support by Giovanni Nunes - https://github.com/plainspooky
+ *                  Emiliano Fraga - https://github.com/efraga-msx
  */
 
 #include <stdio.h>
@@ -21,45 +22,61 @@
 #include "JavaClass.h"
 #include "msx_.h"
 
+// Trace Emiliano
 #define CHECK_FUNC(funct,sig) \
+  printf("msx_ CHECK_FUNC funct: %s\tsig: %s\n", #funct, #sig); \
   if (strcmp(#funct#sig, method_name) == 0) \
   { \
     return generator->msx_##funct##sig(); \
   }
 
+// Trace Emiliano
 #define CHECK_FUNC_CONST(funct,sig) \
+  printf("msx_ CHECK_FUNC_CONST funct: %s\tsig: %s\n", #funct, #sig); \
   if (strcmp(#funct#sig, method_name) == 0) \
   { \
     return generator->msx_##funct##sig(const_val); \
   }
 
+/*
 #define CHECK_FUNC_CONST_2(funct,sig) \
   if (strcmp(#funct#sig, function) == 0) \
   { \
     return generator->msx_##funct##sig(const_val1, const_val2); \
   }
-
+*/
 int msx(JavaClass *java_class, Generator *generator, char *method_name)
 {
   //CHECK_FUNC(someFunction,_I)
-  CHECK_FUNC(beep,_I)
-  CHECK_FUNC(color,_I)
-  CHECK_FUNC(cls,_I)
-  CHECK_FUNC(screen,_I)
-  CHECK_FUNC(keyOn,_I)
-  CHECK_FUNC(keyOff,_I)
-  CHECK_FUNC(fillVRAM,_I)
-  CHECK_FUNC(copyVRAM,_I)
+  CHECK_FUNC(cls,)
+  CHECK_FUNC(beep,)
+  CHECK_FUNC(color,_BBB)
+  CHECK_FUNC(screen,_B)
+  CHECK_FUNC(keyOn,)
+  CHECK_FUNC(keyOff,)
+  CHECK_FUNC(fillVRAM,_III)
+  CHECK_FUNC(copyVRAM,_III)
   CHECK_FUNC(putChar,_C)
-  CHECK_FUNC(setCursor,_I)
+  CHECK_FUNC(setCursor,_BB)
 
   return -1;
 }
 
+/*
+int msx(JavaClass *java_class, Generator *generator, char *method_name, char const_val)
+{
+  //CHECK_FUNC_CONST(someFunction,_C)
+  CHECK_FUNC_CONST(putChar,_C)
+
+  return -1;
+}
+*/
+
+/*
 int msx(JavaClass *java_class, Generator *generator, char *method_name, int const_val)
 {
   //CHECK_FUNC_CONST(someFunction,_I)
-
+  
   return -1;
 }
 
@@ -69,3 +86,4 @@ int msx(JavaClass *java_class, Generator *generator, char *function, int const_v
 
   return -1;
 }
+*/
