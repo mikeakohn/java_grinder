f6339d0f51c09f043b64774cb5b9e14bb904800e
Emiliano Vaz Fraga <efraga@gmail.com>
2016-02-18 02:33:12 -0200

    Adjustments to method CHECK defines and calls. Traces were included.

--- api/msx_.h
+++ api/msx_.h
@@ -8,6 +8,11 @@
  * Copyright 2014-2016 by Michael Kohn
  *
  */
+ 
+/*
+ *   MSX support by Giovanni Nunes - https://github.com/plainspooky
+ *                  Emiliano Fraga - https://github.com/efraga-msx
+ */
 
 #ifndef _MSX_H
 #define _MSX_H
@@ -16,7 +21,10 @@
 #include "JavaClass.h"
 
 int msx(JavaClass *java_class, Generator *generator, char *method_name);
-int msx(JavaClass *java_class, Generator *generator, char *method_name, int const_val);
-int msx(JavaClass *java_class, Generator *generator, char *method_name, int const_val1, int const_val2);
+int msx(JavaClass *java_class, Generator *generator, char *method_name, char const_val);
+
+//int msx(JavaClass *java_class, Generator *generator, char *method_name, int const_val);
+
+//int msx(JavaClass *java_class, Generator *generator, char *method_name, int const_val1, int const_val2);
 
 #endif
