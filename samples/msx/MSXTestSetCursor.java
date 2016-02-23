030ad5277cc7f92f02ba0828fef7e534f292ab2a
Emiliano Vaz Fraga <efraga@gmail.com>
2016-02-22 22:37:45 -0300

    More complex Test Set Cursor Sample

--- samples/msx/MSXTestSetCursor.java
+++ samples/msx/MSXTestSetCursor.java
@@ -26,17 +26,62 @@ public class MSXTestSetCursor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		byte col = 27;
-		byte lin = 15;
-		char c = 'X';
+		byte mode = 0;
+		byte lin = 2;
+		byte col = 2;
+		char c = '0';
 		
-		MSX.setCursor(col, lin);
-		MSX.putChar(c);
+		MSX.screen(mode);
+
+		for (int i = 1; i < 41; i++) 
+		{
+			MSX.setCursor((byte)i, lin);
+			MSX.putChar(c++);
+			
+			if (c > '9') {c = '0';} 
+		}
+
+		lin = 1;
+		c = '0';
+		for (int i = 1; i < 41; i+=10) 
+		{
+			MSX.setCursor((byte)i, lin);
+			MSX.putChar(c++);
+		}
+
+		c = '0';
+		for (int i = 1; i < 25; i++) 
+		{
+			MSX.setCursor(col, (byte)i);
+			MSX.putChar(c++);
+			
+			if (c > '9') {c = '0';} 
+		}
+
+		col = 1;
+		c = '0';
+		for (int i = 1; i < 25; i+=10) 
+		{
+			MSX.setCursor(col, (byte)i);
+			MSX.putChar(c++);
+		}
 		
-		col = 1; lin = 1;
-		MSX.setCursor(col, lin);
-		MSX.putChar(c);
+		c = 'X';
+		lin = 3;
+		for (int i = 1; i < 21; i++) 
+		{
+			MSX.setCursor((byte)i, lin++);
+			MSX.putChar(c);
+		}
+
+		c = 'Y';
 		
+		for (int i = 21; i < 41; i++) 
+		{
+			MSX.setCursor((byte)i, lin--);
+			MSX.putChar(c);
+		}
+
 		while(true);
 	}
 }
