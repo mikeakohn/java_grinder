5cd7ad765179384dfb047433301f8a91d0b62212
deringenieur71 <36e80a8f143606ea5120e8415f6d9d1e3a754e42>
2016-04-17 14:00:41 +0200

    BugFix Z80
    fix swap-bug in int Z80::swap()
    fix hex-bug in int Z80::stack_alu_const(int alu_op, int num)

--- samples/cpc/ScreenCPC.java
+++ samples/cpc/ScreenCPC.java
@@ -4,19 +4,36 @@ public class ScreenCPC
 {
   public static void main(String args[])
   {
-    int x,y,screen;
-    char c;
+    int x,y;
+    // x = 0 to 79
+    // y = 0 to 399
     
     CPC.screen(1);
-    x=20; 
+  
     y=4;
-   
-    screen = 0xc000 + (80 * ((int)(y / 8)));
-    screen += (2048 * (y % 8));
-    screen += x;
     
-    CPC.poke8(screen, (char) 0xEC);
-    
-    c = (char) CPC.readChar();
+    for (x=20; x<60; x++)
+    {
+      CPC.poke8( vmem(x,y), (char) 0x1a);
+      CPC.poke8( vmem(x,y+96), (char) 215);
+    }
+    char c = (char) CPC.readChar();
+	
+	return;
+  }
+  
+  public static int vmem (int x, int y)
+  {
+    int screen,h,u;
+    screen = (y >> 3);
+    screen = (screen << 3); // *8
+    u = screen;
+    screen = (screen << 2); // * 4 = *32
+    screen += u; // = 40*
+    screen = screen << 1; // *2 = 80 *
+    h = (y & (8-1)); // % 8
+    h = h << 11; // *2048
+    screen = screen + h + x + 0xC000;
+    return screen;	  
   }
 }
