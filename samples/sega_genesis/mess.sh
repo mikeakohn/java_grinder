3bfc134d6e3e75bafe8fd1d163c9715e658c8643
Michael Kohn <mike@mikekohn.net>
2019-08-24 05:20:08 -0500

    Change emulator in script to mame instead of mess.

--- samples/sega_genesis/mess.sh
+++ samples/sega_genesis/mess.sh
@@ -12,13 +12,13 @@ then
   extra="-aviwrite out.avi -snapsize 320x216"
 fi
 
-mess genesis -window \
+mame genesis -window \
   -cart sega_genesis_java_demo.bin \
   ${extra}
 
 if [ "$1" = "avi" ]
 then
-  #mencoder -oac mp3lame -ovc x264 -o video.avi -vf scale=640:480 ~/.mess/snap/out.avi
+  #mencoder -oac mp3lame -ovc x264 -o video.avi -vf scale=640:480 ~/.mame/snap/out.avi
   ffmpeg -i snap/out.avi -vf hflip -s 640x480 out.mp4
   ffmpeg -i out.mp4 -vf hflip out2.mp4
 fi
